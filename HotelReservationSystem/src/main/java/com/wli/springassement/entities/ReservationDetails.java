1260e9b4167ca95511264c513bd3c7474b5e921c
A00445457 <wenshuo.li@smu.ca>
2021-07-04 22:33:14 -0300

    change check in check out data type add read me file

--- HotelReservationSystem/src/main/java/com/wli/springassement/entities/ReservationDetails.java
+++ HotelReservationSystem/src/main/java/com/wli/springassement/entities/ReservationDetails.java
@@ -1,15 +1,63 @@
 package com.wli.springassement.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
 import java.util.List;
 
 public class ReservationDetails {
 
 	private int rid;
 	private String hotel_name;
-	private String checkin;
-	private String checkout;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	@JsonFormat(pattern = "yyyy-MM-dd")
+	private Date checkin;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	@JsonFormat(pattern = "yyyy-MM-dd")
+	private Date checkout;
 	private List<Guest> guest_list;
-	String confirm;
+	private String confirm;
+
+	public int getRid() {
+		return rid;
+	}
+
+	public void setRid(int rid) {
+		this.rid = rid;
+	}
+
+	public String getHotel_name() {
+		return hotel_name;
+	}
+
+	public void setHotel_name(String hotel_name) {
+		this.hotel_name = hotel_name;
+	}
+
+	public Date getCheckin() {
+		return checkin;
+	}
+
+	public void setCheckin(Date checkin) {
+		this.checkin = checkin;
+	}
+
+	public Date getCheckout() {
+		return checkout;
+	}
+
+	public void setCheckout(Date checkout) {
+		this.checkout = checkout;
+	}
+
+	public List<Guest> getGuest_list() {
+		return guest_list;
+	}
+
+	public void setGuest_list(List<Guest> guest_list) {
+		this.guest_list = guest_list;
+	}
 
 	public String getConfirm() {
 		return confirm;
@@ -18,38 +66,4 @@ public class ReservationDetails {
 	public void setConfirm(String confirm) {
 		this.confirm = confirm;
 	}
-
-	public int getRid() {
-		return rid;
-	}
-	public void setRid(int rid) {
-		this.rid = rid;
-	}
-	public String getHotel_name() {
-		return hotel_name;
-	}
-	public void setHotel_name(String hotel_name) {
-		this.hotel_name = hotel_name;
-	}
-	public String getCheckin() {
-		return checkin;
-	}
-	public void setCheckin(String checkin) {
-		this.checkin = checkin;
-	}
-	public String getCheckout() {
-		return checkout;
-	}
-	public void setCheckout(String checkout) {
-		this.checkout = checkout;
-	}
-	public List<Guest> getGuest_list() {
-		return guest_list;
-	}
-	public void setGuest_list(List<Guest> guest_list) {
-		this.guest_list = guest_list;
-	}
-	
-	
-	
 }
